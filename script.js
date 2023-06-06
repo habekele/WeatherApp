7cfd7d2a41efdb13f2c9cd1929a7d33a337f1a42
henokbekele <henokbekele1997@gmail.com>
2023-06-06 07:11:46 -0500

    successful API call

---
+++ script.js
@@ -0,0 +1,5 @@
+function callWeather(){
+    fetch("https://api.openweathermap.org/data/2.5/weather?id=5231851&appid=e95e68d847cae52c53bf55dcca4d43b4")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
