a15d91ae9a844ada8ebaf94e2bf4f97fa4c43821
henokbekele <henokbekele1997@gmail.com>
2023-06-18 19:09:23 -0500

    added some json

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function callWeather(){
-    fetch("https://api.openweathermap.org/data/2.5/weather?id=5231851&appid=e95e68d847cae52c53bf55dcca4d43b4")
+let weather;
+
+function callWeather(cityId){
+    var key = "e95e68d847cae52c53bf55dcca4d43b4"
+    var cityId = "5368361"
+    fetch("https://api.openweathermap.org/data/2.5/weather?id="+cityId+"&appid="+key)
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((json) => 
+    {
+        console.log(json.main);
+    });
 }
 
