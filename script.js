c1b4eca70aec7363f057035cf39479548ac9a222
henokbekele <henokbekele1997@gmail.com>
2023-07-07 16:02:30 -0500

    made the json display on the page

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 let weather;
+let description;
+let wind;
+let humidity;
+var APIKey = "e95e68d847cae52c53bf55dcca4d43b4"
 
 function callWeather(){
-    var APIKey = "e95e68d847cae52c53bf55dcca4d43b4"
-    const city =document.getElementById('#search-bar input').value;
+    var city =document.querySelector("#search-bar").value;
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
     .then((response) => response.json())
-    .then((json) => 
+    .then((data) => 
     {
-        console.log(json.main);
-    });
-}
+        console.log(data);
+        weather = ((data.main.temp) * 9/5) + 32;
+        description = data.weather[0].description;
+        humidity = data.main.humidity;
+        wind = data.wind.speed;
+        document.querySelector(".temperature").innerHTML = weather;
+        document.querySelector(".description").innerHTML = description;
+        document.querySelector(".humidity").lastElementChild.lastElementChild.innerHTML = "Humidity: "+humidity;
+        document.querySelector(".wind").lastElementChild.lastElementChild.innerHTML = "Wind Speed: "+wind;
 
+    });
+
+}
