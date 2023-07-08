2ff9225805c49d8a0589e30dd47f84c7694d013a
henokbekele <henokbekele1997@gmail.com>
2023-07-08 00:56:34 -0500

    made the json display on the page

--- script.js
+++ script.js
@@ -3,29 +3,36 @@ let description;
 let wind;
 let humidity;
 let arr;
+let city;
+let country;
 var APIKey = "e95e68d847cae52c53bf55dcca4d43b4"
 
-function callWeather(){
-    var city =document.querySelector("#search-bar").value;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
-    .then((response) => response.json())
-    .then((data) => 
-    {
-        
-        arr = (Object.entries(data));
-        console.log(arr);
-        var cel = (arr[3][1].temp);
+function ChangeWeather(data){
+   
+        var cel = data.main.temp;
+        description = data.weather[0].description;
+        humidity = data.main.humidity;
+        wind = data.wind.speed;
+        city = data.name;
+        country = data.sys.country;
         weather = Math.floor((cel * 9/5) + 32);
 
-        description = arr[1][1][0].description;
-        humidity = arr[3][1].humidity;
-        wind = arr[5][1].speed;
-
+        document.querySelector(".city").innerHTML = city+", "+country;
         document.querySelector(".temperature").innerHTML = weather;
         document.querySelector(".description").innerHTML = description;
         document.querySelector(".humidity").lastElementChild.innerHTML = "Humidity: "+humidity+"%";
         document.querySelector(".wind").lastElementChild.innerHTML = "Wind Speed: "+wind+" mph";
+        document.querySelector("#search-bar").value = "";
+
+}
+function search(){
+    var input = document.querySelector("#search-bar").value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${APIKey}`)
+    .then((response) => response.json())
+    .then((data) => 
+    { 
+        console.log(data);
+        ChangeWeather(data);
 
     });
-
 }
